0ee195c940f3a68959180c68f1477a1dec07f0e8
Aswathi1302 <aswathisomaraj1302@gmail.com>
2022-11-07 09:28:59 +0530

    first commit

---
+++ sensorvalues.py
@@ -0,0 +1,15 @@
+while True:
+    print("select an option from the menu")
+    print("1. add values")
+    print("2.view values")
+    print("3. search item by date")
+    print("4.exit")
+    choice=int(input("enter your choice:-"))
+    if(choice==1):
+        print("ADD VALUES")
+    elif(choice==2):
+        print("VIEW ALL VALUES")
+    elif(choice==3):
+        print("SEARCH VALUE BY DATE")
+    elif(choice==4):
+        break            
